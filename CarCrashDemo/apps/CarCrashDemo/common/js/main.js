4494dda47c48426f5d09822a88504db921200b4e
dramirezh <dramirez@solutiaintelligence.com>
2015-01-09 20:45:41 -0600

    login y ajuste de app

--- CarCrashDemo/apps/CarCrashDemo/common/js/main.js
+++ CarCrashDemo/apps/CarCrashDemo/common/js/main.js
@@ -1,16 +1,15 @@
 function wlCommonInit(){
-	/*
-	 * Use of WL.Client.connect() API before any connectivity to a MobileFirst Server is required. 
-	 * This API should be called only once, before any other WL.Client methods that communicate with the MobileFirst Server.
-	 * Don't forget to specify and implement onSuccess and onFailure callback functions for WL.Client.connect(), e.g:
-	 *    
-	 *    WL.Client.connect({
-	 *    		onSuccess: onConnectSuccess,
-	 *    		onFailure: onConnectFailure
-	 *    });
-	 *     
-	 */
-	
-	// Common initialization code goes here
-	
+}
+function logIn()
+{
+	var mail = $('#txtEmail').val();
+	var pass = $('#txtPwd').val();
+	if(mail != "admin" || pass != "admin")
+	{
+		alert('Fallo en Log In');
+	}
+	else
+	{
+		location.href = "#perfil";
+	}
 }
