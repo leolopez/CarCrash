9d0a76a423bdf95a963679897ce481672a32b4db
dramirezh <dramirez@solutiaintelligence.com>
2015-01-13 13:18:53 -0600

    account js and login ajustment

--- CarCrashDemo/apps/CarCrashDemo/common/js/main.js
+++ CarCrashDemo/apps/CarCrashDemo/common/js/main.js
@@ -1,15 +1,2 @@
 function wlCommonInit(){
 }
-function logIn()
-{
-	var mail = $('#txtEmail').val();
-	var pass = $('#txtPwd').val();
-	if(mail != "admin" || pass != "admin")
-	{
-		alert('Fallo en Log In');
-	}
-	else
-	{
-		location.href = "#perfil";
-	}
-}
